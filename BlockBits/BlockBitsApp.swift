82ed742c7902a6d4ea6dcae618ee2e84de239bd2
Engels Tercero <higashikataengels@icloud.com>
2023-01-09 16:02:45 -0600

    Initial Commit

---
+++ BlockBits/BlockBitsApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlockBitsApp.swift
+//  BlockBits
+//
+//  Created by Higashikata Maverick on 9/1/23.
+//
+
+import SwiftUI
+
+@main
+struct BlockBitsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BlockBits/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  BlockBits
+//
+//  Created by Higashikata Maverick on 9/1/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
