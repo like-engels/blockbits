b8b316eddcb0f833ecbb4a0ed736db8368ccc3fc
Engels Tercero <higashikataengels@icloud.com>
2023-01-09 18:03:04 -0600

    first commit

---
+++ BlockBits/Tools/ApolloClient.swift
@@ -0,0 +1,11 @@
+//
+//  ApolloClient.swift
+//  BlockBits
+//
+//  Created by Higashikata Maverick on 9/1/23.
+//
+
+import Foundation
+import Apollo
+
+let graphQLClient = ApolloClient(url: URL(string: "https://rickandmortyapi.com/graphql")!)
